784d1b58350cd1dcac56eeb686605fdad69cf9e3
AshokParmar24 <ashokparmar72038@gmail.com>
2025-01-05 10:48:24 +0530

    change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 import theme from "@/config/theme";
-import '@mantine/core/styles.css';
-
+import "@mantine/core/styles.css";
+import Header from "@/components/Header/Header";
 
 export default function App({ Component, pageProps }) {
   return (
     <MantineProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
     </MantineProvider>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,5 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Dashboard />
-    </>
-  );
+  return <Dashboard />;
 }
