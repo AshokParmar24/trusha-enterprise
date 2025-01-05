09e2e7557ccc5c0dfcb86d2ab19a4fc72aa2199c
AshokParmar24 <ashokparmar72038@gmail.com>
2025-01-05 12:27:37 +0530

    remove-header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,10 @@
-import Header from "@/components/Layout/Header";
+// import Header from "@/components/Layout/Header";
 import Footer from "@/components/Layout/Footer";
 
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
+      {/* <Header /> */}
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
